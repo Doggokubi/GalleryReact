a5b6ee2b9701b3cfff8c00b7ad6e1327f550a496
Doggokubi <fran_cikk@wp.pl>
2025-04-29 11:35:38 +0200

    done

--- src/imageloader.jsx
+++ src/imageloader.jsx
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ImageLoader = (props) => {
   const category = props.category;
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null); // przechowywanie wybranego zdjęcia w modal
 
   useEffect(() => {
     const fetchImages = async () => {
       try {
-        const response = await axios.get(`https://picsum.photos/v2/list?page=${category}&limit=12   `);
+        const response = await axios.get(
+          `https://picsum.photos/v2/list?page=${category}&limit=12`
+        );
         setImages(response.data);
         setLoading(false);
       } catch (err) {
@@ -30,15 +33,46 @@ const ImageLoader = (props) => {
     return <div>Błąd: {error}</div>;
   }
 
+  const handleImageClick = (imageUrl) => {
+    setSelectedImage(imageUrl);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div>
-      <div>
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
         {images.map((image, index) => (
           <div key={index}>
-            <img src={image.download_url} alt={`Obrazek ${index}`}/>
+            <img
+              src={image.download_url}
+              alt={`Obrazek ${index}`}
+              onClick={() => handleImageClick(image.download_url)}
+              style={{
+                cursor: "pointer",
+                width: "150px",
+                height: "auto",
+                objectFit: "cover",
+              }}
+            />
           </div>
         ))}
       </div>
+
+      {/* Modal */}
+      {selectedImage && (
+        <div className="modal" onClick={handleCloseModal} style={{width: "50%", marginLeft: "25%"}}>
+          <img
+            src={selectedImage}
+            alt="Podgląd"
+            style={{
+            height: "600px",
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
